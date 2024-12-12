cc6e2478923f027fc23344d6fca14d0d638d0551
AndrianDaniel <andrian.dorin.daniel@gmail.com>
2024-12-12 22:25:41 +0200

    added timer sessions and resetting the application

--- main.py
+++ main.py
@@ -10,14 +10,35 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset_timer():
+	window.after_cancel(timer)
+	canvas.itemconfig(timer_text, text="00:00")
+	title_label.config(text="Timer")
+	check_marks_label.config(text="")
+	global reps
+	reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-	count_down(5 * 60)
+	global reps
+	reps += 1
+	work_sec = 1 * 5
+	short_break_sec = 1 * 5
+	long_break_sec = 1 * 5
 
+	if reps % 8 == 0:
+		count_down(long_break_sec)
+		title_label.config(text="LBreak", fg=RED)
+	elif reps % 2 == 0:
+		count_down(short_break_sec)
+		title_label.config(text="SBreak", fg=PINK)
+	else:
+		count_down(work_sec)
+		title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -28,8 +49,17 @@ def count_down(count):
 		count_sec = f'0{count_sec}'
 
 	canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
-	window.after(1000, count_down, count - 1)
 
+	if count > 0:
+		global timer
+		timer = window.after(1000, count_down, count - 1)
+	else:
+		start_timer()
+		marks = ""
+		work_sessions = math.floor(reps/2)
+		for _ in range(work_sessions):
+			marks += "✔"
+		check_marks_label.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -42,16 +72,16 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-timer_label.grid(row=0, column=1)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+title_label.grid(row=0, column=1)
 
 start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(row=2, column=2)
 
-check_marks_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+check_marks_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
 check_marks_label.grid(row=3, column=1)
 
 window.mainloop()
